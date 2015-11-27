66b96a31c098090141602a4aa8877ac4ffcd32de
Will Liu <daodao1990@gmail.com>
2015-11-27 13:29:22 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo git clone https://github.com/ma6174/speak_raspi_ip.git
+sudo git clone https://github.com/lazydao/speak_raspi_ip.git
 sudo apt-get install -y mpg123
 sudo rm -rf /var/speak_raspi_ip/
 sudo mv speak_raspi_ip /var/
